f91c8375934a9c1e88b13b6f6eb9dee9ee292fdf
dh3eraj <dheerajgupta200002@gmail.com>
2025-02-22 12:36:05 +0530

    mc

---
+++ integration_test/app_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:silent_moon/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end to end test', () {
+    testWidgets('fff', (tester) async {
+      app.main();
+
+      await tester.pumpAndSettle();
+      tester.tap(find.byType(FloatingActionButton));
+     await Future.delayed(Duration(seconds: 5));
+      await tester.pumpAndSettle();
+    });
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text(
+              key: Key('count-value'),
               '${_counter.count}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
